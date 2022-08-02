3858a6304d2fb0d55dcc5b997b869c71b96359b2
shin <62875437+syb0228@users.noreply.github.com>
2022-08-02 08:21:23 +0000

    graph

--- Algorithm/topologySort.py
+++ Algorithm/topologySort.py
@@ -2,10 +2,8 @@ from collections import deque
 
 # 노드의 개수와 간선의 개수 입력 받기
 v, e = map(int, input().split())
-
 # 모든 노드에 대한 진입차수는 0으로 초기화
 indegree = [0] * (v + 1)
-
 # 각 노드에 연결된 간선 정보를 담기 위한 연결 리스트 초기화
 graph = [[] for i in range(v + 1)]
 
@@ -24,7 +22,6 @@ def topology_sort():
   for i in range(1, v + 1):
     if indegree[i] == 0:
       q.append(i)
-
   # 큐가 빌 때까지 반복
   while q:
     # 큐에서 원소 꺼내기
